33a62e066999c9d486fe4bae036d0ef4dd8a725c
Steven George <stoveng@users.noreply.github.com>
2020-12-28 21:19:44 -0800

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+binDir = "bin/"
+tempDir = "temp/"
+
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir (binDir .. outputDir)
+	objdir (tempDir .. outputDir)
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
